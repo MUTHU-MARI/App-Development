8dd6cbb06d157f5540d42b4636e0a35dc055c2b1
Muthumari <110532427+MUTHU-MARI@users.noreply.github.com>
2023-09-25 13:16:14 +0530

    Add files via upload

---
+++ App Development/Day 8/src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+    {
+        pid : 0,
+        title : "Photo Frame",
+        price : 450,
+        desc : "Capture and cherish your memories in style with our personalized photo frames.Frame your moments, frame your story – custom photo frames, just for you",
+        img : "https://m.media-amazon.com/images/I/41R-HJ7ECeL._SX300_SY300_QL70_FMwebp_.jpg"
+    },
+    {
+        pid : 1,
+        title : "Watch",
+        price : 2800,
+        desc : "Elevate your wrist game with our customized watches – timeless style, your way.Crafted for uniqueness, our personalized watches are more than just accessories",
+        img : "/assets/images/watch.jpeg"
+    },
+    {
+        pid : 2,
+        title : "shoe",
+        price : 1500,
+        desc : "Step into individuality with our customized shoes, designed to match your style.Walk confidently in shoes that are as unique as you are.",
+        img : "https://www.superkicks.in/cdn/shop/files/CHUCK_TAYLOR_ALL_STAR_CONSTRUCT_HI.png?v=1694689277"
+    },
+    {
+        pid : 3,
+        title : "Mug",
+        price : 550,
+        desc : "Sip in style with personalized mugs that brighten your day.Create lasting memories with every cup, customized just for you.",
+        img : "/assets/images/mug.jpeg"
+    },
+    {
+        pid : 4,
+        title : "kids pillows",
+        price : 400,
+        desc : "Upgrade your comfort with our premium pillows for a better night's sleep.Experience the perfect blend of support and luxury with our pillows.",
+        img : "/assets/images/pillow.jpeg"
+    },
+    {
+        pid : 5,
+        title : "printed shirts",
+        price : 1250,
+        desc : "Elevate your style with personalized shirts that reflect your uniqueness.Stand out in custom-designed shirts tailored to your taste.",
+        img : "/assets/images/shirt.jpeg"
+    }
+]
+
+export default DATA;
